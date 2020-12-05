84d5f4d2aa7a0e2bd674d3544c7ece8fbdae4d44
Oona Laitamaki <oona.laitamaki@tuni.fi>
2020-12-05 23:58:57 +0200

    Skip test cases under development

--- test/integration-tests/calculations.test.js
+++ test/integration-tests/calculations.test.js
@@ -2,7 +2,7 @@ import assert from 'assert';
 import chai from 'chai';
 var expect = chai.expect;
 
-//add paths here for tests
+// Add paths here for tests
 import add from '../../src/add.js';
 import countBy from'../../src/countBy.js';
 import divide from '../../src/divide.js';
@@ -13,12 +13,16 @@ describe('Tests library used with different calculations', () => {
         expect(result).to.equal(1);
     });
 
-    //This fails because divide returns 1
+    it('(1 - 2) / 3', () => {
+        const result = divide(add(1,-2), 3);
+        expect(result).to.equal(1);
+    });
+
+    // This fails because divide returns 1
     it.skip('1 / 4 + 4', () => {
-        const divideResult = divide(1,4);
-        expect(divideResult).to.equal(0.25);
-        const result = add(divideResult, 4);
+        const result = add(divide(1,4), 4);
         expect(result).to.equal(4.25);
     });
+
 });
 

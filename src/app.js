bbfcb7153466b16310d44bf76ee4c3cd5ab171ed
Ville <villeuhlgren@gmail.com>
2020-12-05 20:05:26 +0200

    unit testing by ville

--- src/app.js
+++ src/app.js
@@ -1,13 +1,28 @@
 //This is a test app file
 
-const add = (a,b) => a + b;
+const add = (a = 0,b = 0) => {
+
+if(typeof a!=='number' || typeof b !== 'number'){
+    return 0;
+}    
+
+return a + b;
+
+};
 
 //you could add more stuff here
-
-
+const sub = (a = 0, b = 0) => {
+if(typeof a!=='number' || typeof b !== 'number'){
+    return 0;
+}    
+    
+    return a - b;
+    
+};
 
 module.exports = {
 
 add,
+sub
 
 };
--- test/app.spec.js
+++ test/app.spec.js
@@ -4,14 +4,59 @@
 //add more tests. But you will nee to modify either this file 
 //or include everything into a singular file
 
+//This contain unit testing
 const assert = require('assert');
+const { expect } = require('chai');
 //add paths here for tests
 const { add } = require('../src/app'); //Unit testing, you could add other test same way
+const { sub } = require('../src/app')
 
 describe('this is the add function', () => {
     it('lets add two numbers together', () => {
         const result = add(2,2);
         //now you can add all kinds of things what you want to do
-        assert.strictEqual(result, 4);//if equal
-    })
-})
+        //assert.strictEqual(result, 4);//if equal
+        expect(result).to.equal;
+        
+        //assert.ifError("There were an error");
+    });
+    it('One number hadling', () => {
+        const result = add(2);
+        expect(result).to.be.equal(2);
+
+    });
+    it('Handling zero'), () => {
+        const result = add();
+        expect(result).to.be.equal(0);
+    }
+    it('Returns 0 if either argument is not a number, if it is string'), () => {
+     const result = add(2, 'testing');
+     expect(result).to.be.eq(0);
+    }
+});
+
+//sub
+
+describe('this is the sub function', () => {
+    it('lets sub two numbers together', () => {
+        const result = sub(5,2);
+        //now you can add all kinds of things what you want to do
+        //assert.strictEqual(result, 4);//if equal
+        expect(result).to.equal;
+        
+        //assert.ifError("There were an error");
+    });
+    it('One number hadling', () => {
+        const result = sub(2);
+        expect(result).to.be.equal(2);
+
+    });
+    it('Handling zero'), () => {
+        const result = sub();
+        expect(result).to.be.equal(0);
+    }
+    it('Returns 0 if either argument is not a number, if it is string'), () => {
+     const result = sub(2, 'testing');
+     expect(result).to.be.equal(0);
+    }
+});

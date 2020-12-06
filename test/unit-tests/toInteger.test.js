fe56dc8a01561a020ecf5f52dcbfd2d824d93a64
Oona Laitamaki <oona.laitamaki@tuni.fi>
2020-12-06 21:40:14 +0200

    Test if CI pipeline works fine

--- test/unit-tests/toInteger.test.js
+++ test/unit-tests/toInteger.test.js
@@ -9,7 +9,6 @@ describe('Testing function toInteger', () => {
         expect(result).to.have.equal(1);
     });
 
-    
     it.skip('Should convert 1.9 to 2', () => {
         const result = toInteger(1.9);
         expect(result).to.have.equal(2);

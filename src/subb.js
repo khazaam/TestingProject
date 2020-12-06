11d3a31879c131f935b16006765c48a57eade52d
Ville <villeuhlgren@gmail.com>
2020-12-06 11:45:24 +0200

    substraction added

---
+++ src/subb.js
@@ -0,0 +1,25 @@
+import createMathOperation from './.internal/createMathOperation.js'
+
+/**
+ * subbstracts two numbers.
+ *
+ * @since 3.4.0
+ * @category Math
+ * @param {number} augend The first number in an addition.
+ * @param {number} addend The second number in an addition.
+ * @returns {number} Returns the total.
+ * @example
+ *
+ * subb(6, 4)
+ * // => 10
+ */
+
+/**
+ * 
+ * This code is our made by us, we wanted to have substraction operation
+ * 
+ */
+
+const subb = createMathOperation((augend, addend) => augend - addend, 0)
+
+export default subb
--- test/app.spec.js
+++ test/app.spec.js
@@ -3,6 +3,8 @@ import chai from 'chai';
 var expect = chai.expect;
 import add from '../src/add.js';
 
+//substraction this is our own code, we saw this in couple tutorials to have for integration tests.
+import subb from '../src/subb.js';
 
 describe('this is the add function', () => {
     it('lets add two numbers together', () => {
@@ -30,9 +32,9 @@ describe('this is the add function', () => {
 
 //sub
 
-describe('this is the sub function', () => {
+describe('this is the subb function', () => {
     it('lets sub two numbers together', () => {
-        const result = sub(5,2);
+        const result = subb(5,2);
         //now you can add all kinds of things what you want to do
         //assert.strictEqual(result, 4);//if equal
         expect(result).to.equal;
@@ -40,16 +42,16 @@ describe('this is the sub function', () => {
         //assert.ifError("There were an error");
     });
     it('One number hadling', () => {
-        const result = sub(2);
+        const result = subb(2);
         expect(result).to.be.equal(2);
 
     });
     it('Handling zero'), () => {
-        const result = sub();
+        const result = subb();
         expect(result).to.be.equal(0);
     }
     it('Returns 0 if either argument is not a number, if it is string'), () => {
-     const result = sub(2, 'testing');
+     const result = subb(2, 'testing');
      expect(result).to.be.equal(0);
     }
 });
